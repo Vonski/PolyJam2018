727defdc81ba2e41e460cf2cddd7d1eedcde4705
Unknown <daniellodelapedro@interia.pl>
2018-01-28 00:18:52 +0100

    Working curves

--- PolyJam/Assets/Scripts/CameraController.cs
+++ PolyJam/Assets/Scripts/CameraController.cs
@@ -2,25 +2,106 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum cameraDirection { UP, RIGHT, DOWN, LEFT };
+
 public class CameraController : MonoBehaviour {
 
-    float y_pos;
+    public cameraDirection direction;
+    public bool transition;
+    public float lerpProgress;
+    public float xConst, yConst;
+    float yPos, xPos;
+    public float xStop, yStop;
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        yStop = 1000.0f;
+        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+        foreach (GameObject p in players)
+        {
+            if (p.transform.position.y < yPos)
+                yPos = p.transform.position.y;
+        }
+        yStop = yPos;
+        xStop = 0.0f;
+        transition = false;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        y_pos = 10000.0f;
-        GameObject [] players = GameObject.FindGameObjectsWithTag("Player");
-        foreach(GameObject p in players)
+        if(direction == cameraDirection.UP)
         {
-            if (p.transform.position.y < y_pos)
-                y_pos = p.transform.position.y;
+            yPos = 10000.0f;
+            GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+            foreach (GameObject p in players)
+            {
+                if (p.transform.position.y < yPos)
+                    yPos = p.transform.position.y;
+            }
+            yPos += 2.5f;
+            if (yStop < yPos)
+                yStop = yPos;
+            yPos = yStop;
+            xPos = xConst;
         }
-        transform.position = new Vector3(transform.position.x, y_pos, -10);
+        if (direction == cameraDirection.RIGHT)
+        {
+            xPos = 10000.0f;
+            GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+            foreach (GameObject p in players)
+            {
+                if (p.transform.position.x < xPos)
+                    xPos = p.transform.position.x;
+            }
+            xPos += 3.5f;
+            if (xStop < xPos)
+                xStop = xPos;
+            xPos = xStop;
+            yPos = yConst;
+        }
+        if (direction == cameraDirection.DOWN)
+        {
+            yPos = -10000.0f;
+            GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+            foreach (GameObject p in players)
+            {
+                if (p.transform.position.y > yPos)
+                    yPos = p.transform.position.y;
+            }
+            yPos -= 2.5f;
+            if (yStop > yPos)
+                yStop = yPos;
+            yPos = yStop;
+            xPos = xConst;
+        }
+        if (direction == cameraDirection.LEFT)
+        {
+            xPos = -10000.0f;
+            GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+            foreach (GameObject p in players)
+            {
+                if (p.transform.position.x > xPos)
+                    xPos = p.transform.position.x;
+            }
+            xPos -= 3.5f;
+            if (xStop > xPos)
+                xStop = xPos;
+            xPos = xStop;
+            yPos = yConst;
+        }
+
+
+        if (transition)
+        {
+            transform.position = Vector3.Lerp(transform.position, new Vector3(xPos,yPos,-10), lerpProgress);
+            lerpProgress += 0.002f;
+            if (lerpProgress >= 1.0f)
+                transition = false;
+        }
+        else
+        {
+            transform.position = new Vector3(xPos, yPos, -10);
+        }
 
 	}
 }
---
+++ PolyJam/Assets/Scripts/CameraDirectionChangeController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraDirectionChangeController : MonoBehaviour {
+
+    public cameraDirection myDirectionChange;
+    GameObject myCamera;
+    GameObject[] players;
+    int counter;
+
+	// Use this for initialization
+	void Start () {
+        myCamera = GameObject.FindGameObjectWithTag("MainCamera");
+        players = GameObject.FindGameObjectsWithTag("Player");
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag=="Player")
+        {
+            counter++;
+            if(counter == players.Length)
+            {
+                myCamera.GetComponent<CameraController>().transition = true;
+                myCamera.GetComponent<CameraController>().direction = myDirectionChange;
+                myCamera.GetComponent<CameraController>().lerpProgress = 0.0f;
+                if (myDirectionChange == cameraDirection.LEFT || myDirectionChange == cameraDirection.RIGHT)
+                {
+                    myCamera.GetComponent<CameraController>().yConst = transform.position.y;
+                    if (myDirectionChange == cameraDirection.LEFT)
+                        myCamera.GetComponent<CameraController>().xStop = transform.position.x - 3.5f;
+                    else
+                        myCamera.GetComponent<CameraController>().xStop = transform.position.x + 3.5f;
+
+                }
+                if (myDirectionChange == cameraDirection.UP || myDirectionChange == cameraDirection.DOWN)
+                {
+                    myCamera.GetComponent<CameraController>().xConst = transform.position.x;
+                    if (myDirectionChange == cameraDirection.UP)
+                        myCamera.GetComponent<CameraController>().yStop = transform.position.y - 2.5f;
+                    else
+                        myCamera.GetComponent<CameraController>().yStop = transform.position.y + 2.5f;
+
+                }
+            }
+        }
+    }
+}
