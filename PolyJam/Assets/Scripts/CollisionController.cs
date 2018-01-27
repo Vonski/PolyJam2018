a39a7185d0aa467142854cc6fec346d33cec6954
Unknown <lidka.wojciechowska1996@gmail.com>
2018-01-27 17:48:16 +0100

    players collisions

---
+++ PolyJam/Assets/Scripts/CollisionController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("hehe");
+        if (collision.gameObject.tag == "Player")
+            Debug.Log("player");
+        else if (collision.gameObject.tag == "Obstacle")
+            Debug.Log("Obstacle");
+        
+
+    }
+}
--- PolyJam/Assets/Scripts/TimeController.cs
+++ PolyJam/Assets/Scripts/TimeController.cs
@@ -4,27 +4,36 @@ using UnityEngine;
 using UnityEngine.UI;
 using System.Text;
 
-public class TimeController : MonoBehaviour {
+public class TimeController : MonoBehaviour
+{
     Text timeview;
     bool game_end = false;
     float start_time, current_time;
-    public float game_duration=120.0f;
-	// Use this for initialization
-	void Start () {
+    public float game_duration = 120.0f;
+    // Use this for initialization
+    void Start()
+    {
         timeview = GameObject.Find("TimeToEnd").GetComponent<Text>();
         start_time = Time.time;
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         current_time = Time.time;
         timeview.text = (game_duration - (current_time - start_time)).ToString("0");
         if (current_time - start_time < game_duration)
-            game_end = true;		
-	}
+            game_end = true;
+    }
 
     bool getGameEnd()
     {
         return game_end;
     }
+    void setGameEnd(bool val)
+    {
+        game_end = val;
+    }
+
 }
+
